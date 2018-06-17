19813ef309fada3ab1fc1ebef8a6a8d80a795a7e
Per-Øyvind Bruun <per-oyvind.bruun@vitari.no>
2018-06-17 14:27:55 +0200

    Init

---
+++ NLog.AzureLog/Azure.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLog.AzureLog
+{
+    [Target("Azure")]
+    public sealed class Azure: TargetWithLayout
+    {
+        [RequiredParameter]
+        public string CustomerId { get; set; }
+        public string SharedKey { get; set; }
+        public string LogName { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = this.Layout.Render(logEvent);
+
+            // Create a hash for the API signature
+            var datestring = DateTime.UtcNow.ToString("r");
+            var jsonBytes = Encoding.UTF8.GetBytes(logMessage);
+            string stringToHash = "POST\n" + jsonBytes.Length + "\napplication/json\n" + "x-ms-date:" + datestring + "\n/api/logs";
+            string hashedString = BuildSignature(stringToHash, this.SharedKey);
+            string signature = "SharedKey " + this.CustomerId + ":" + hashedString;
+
+            PostData(signature, datestring, logMessage);
+
+
+        }
+
+        public string BuildSignature(string message, string secret)
+        {
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = Convert.FromBase64String(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hash = hmacsha256.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public void PostData(string signature, string date, string json)
+        {
+            try
+            {
+                string url = "https://" + CustomerId + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";
+
+                System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("Log-Type", LogName);
+                client.DefaultRequestHeaders.Add("Authorization", signature);
+                client.DefaultRequestHeaders.Add("x-ms-date", date);
+                client.DefaultRequestHeaders.Add("time-generated-field", "");
+
+                System.Net.Http.HttpContent httpContent = new StringContent(json, Encoding.UTF8);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                Task<System.Net.Http.HttpResponseMessage> response = client.PostAsync(new Uri(url), httpContent);
+
+                System.Net.Http.HttpContent responseContent = response.Result.Content;
+                string result = responseContent.ReadAsStringAsync().Result;
+                Console.WriteLine("Return Result: " + result);
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine("API Post Exception: " + excep.Message);
+            }
+        }
+    }
+}
